74c350256f93a344c644daa33290e4cec260e252
Alex Barashkov <lnikell@gmail.com>
2020-06-26 17:05:06 +0200

    Fixed bug

--- src/components/image-drop/image-drop.js
+++ src/components/image-drop/image-drop.js
@@ -1,44 +1,51 @@
-import React, { useState } from 'react';
-import { useDropzone } from 'react-dropzone';
-import classNames from 'classnames/bind';
-import styles from './image-drop.module.scss';
+import React, { useState } from "react"
+import { useDropzone } from "react-dropzone"
+import classNames from "classnames/bind"
+import styles from "./image-drop.module.scss"
 
-
-const ImageDrop = (props) => {
-  const [files, setFiles] = useState([]);
+const ImageDrop = props => {
+  const [files, setFiles] = useState([])
   const { acceptedFiles, getRootProps, isDragActive } = useDropzone({
-    accept: 'image/png,image/jpg,image/jpeg',
-    onDrop: (acceptedFiles) => {
-      setFiles(acceptedFiles.map((file) => Object.assign(file, {
-        preview: URL.createObjectURL(file),
-      })));
+    accept: "image/png,image/jpg,image/jpeg",
+    onDrop: acceptedFiles => {
+      setFiles(
+        acceptedFiles.map(file =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      )
     },
-  });
-  const cx = classNames.bind(styles);
-  const message = isDragActive ? 'Drop it' : "Drag 'n' drop your x2 design here";
-  const thumbs = files.map((file) => (
+  })
+  const cx = classNames.bind(styles)
+  const message = isDragActive ? "Drop it" : "Drag 'n' drop your x2 design here"
+  const thumbs = files.map(file => (
     <div key={file.name}>
       <img
         src={file.preview}
+        srcSet={`${file.preview} 2x`}
         alt={file.name}
         className={styles.img}
       />
     </div>
-  ));
+  ))
 
   return (
     <>
-      {files.length === 0
-        ? (
-          <div className={cx('container', { active: isDragActive })} {...getRootProps()}>
-            <div>
-              <div>{message}</div>
-            </div>
+      {files.length === 0 ? (
+        <div
+          className={cx("container", { active: isDragActive })}
+          {...getRootProps()}
+        >
+          <div>
+            <div>{message}</div>
           </div>
-        )
-        : <>{thumbs}</>}
+        </div>
+      ) : (
+        <>{thumbs}</>
+      )}
     </>
-  );
-};
+  )
+}
 
-export default ImageDrop;
+export default ImageDrop
